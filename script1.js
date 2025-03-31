e17bd67d9fc0dbc9461b6c09cae41ca8c5dddb98
snm62 <sandra@seattlenewmedia.com>
2025-04-01 02:37:43 +0530

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,9 @@
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "https://cdn.jsdelivr.net/gh/snm62/consentbit@fa18a25/consentbit.css";
+document.head.appendChild(link);
+2:36
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "https://cdn.jsdelivr.net/gh/snm62/consentbit@fa18a25/consentbit.css";
+document.head.appendChild(link);
